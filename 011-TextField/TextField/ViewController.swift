a04af0b9437ff6769d52e965df8739dc34c2a4b2
Roberts Kursitis <53969619+Automata02@users.noreply.github.com>
2023-03-16 16:17:42 +0200

    TextField testing
    Continuation of textField testing

--- 011-TextField/TextField/ViewController.swift
+++ 011-TextField/TextField/ViewController.swift
@@ -7,13 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+	
+	@IBOutlet private(set) var usernameField: UITextField!
+	@IBOutlet private(set) var passwordField: UITextField!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
 	}
+	
+	//prevents user from entering spaces in to the textfield
+	func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange,
+				   replacementString string: String) -> Bool { if textField === usernameField {
+		return !string.contains(" ") } else {
+			return true
+		}
+	}
 
-
+	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+		if textField === usernameField {
+			passwordField.becomeFirstResponder()
+		} else {
+			guard let username = usernameField.text, let password = passwordField.text else {
+				return false
+			}
+			passwordField.resignFirstResponder()
+			performLogin(username: username, password: password)
+		}
+		return true
+	}
+	
+	func performLogin(username: String, password: String) {
+		print("Username: \(username)")
+		print("Password: \(password)")
+	}
 }
 
